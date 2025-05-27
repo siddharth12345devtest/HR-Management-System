4bf8357a5e0b9f1798d576d465717aedb6d9e72f
Srinivas <Likhi-Lap01@Likhitech-Lap01>
2025-05-27 16:47:43 +0530

    dashboard.component updated

--- src/app/components/dashboard/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,32 @@
 import { Component, AfterViewInit } from '@angular/core';
-
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const ctx: any = document.getElementById('attendanceChart');
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['Present', 'Late', 'Permission', 'Absent'],
+        datasets: [{
+          data: [59, 21, 2, 15],
+          backgroundColor: ['#0d6efd', '#ffc107', '#17a2b8', '#dc3545'],
+          borderWidth: 0
+        }]
+      },
+      options: {
+        cutout: '70%',
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+  }
 }
