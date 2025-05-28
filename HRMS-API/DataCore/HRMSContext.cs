5a33873aea8c4146bfc5668a414eea2ece714506
SiddharthDevTest <siddharthdevtest12345@gmail.com>
2025-05-28 15:35:17 +0530

    API setup

---
+++ HRMS-API/DataCore/HRMSContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DataCore
+{
+    public class HRMSContext : DbContext
+    {
+        public HRMSContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
